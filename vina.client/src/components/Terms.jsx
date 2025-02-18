3d882d7576a3f156f9848f56136d60eb88dad924
igor <Igor_Ivanic@hotmail.com>
2025-02-18 18:27:33 +0100

    ..

--- vina.client/src/components/Terms.jsx
+++ vina.client/src/components/Terms.jsx
@@ -1,105 +1,106 @@
 function Terms({ locale, direction, onLocaleChange }) {
     return (
-    <>
+        <>
+            <br /> <br /><br />
+            <hr />
+            <br /> <br /><br />
+            <h3>Uvijeti</h3>
+
+            <p><strong><u>Cijene</u></strong><u><br /></u>Cijene navedene na webu su maloprodajne, sa uključenim PDV-om i povratnom naknadom od 0,50 kn, izražene u eurima (EUR).</p>
+
+            <p>Prodavatelj ( Vinarija Ivanić  ) je ovlašten mijenjati cijene bez prethodne obavijesti, kao što je ovlašten bez prethodne obavijesti mijenjati cijene isključivo za webshop. Također, Prodavatelj je ovlašten bez prethodne obavijesti odrediti popust, dnevne ili tjedne akcije za pojedine proizvode. Navedene pogodnosti mogu biti isključivo za web kupovinu što će kupcu biti naznačeno prije same kupovine.</p>
 
 
-<h3>terms</h3>
-    
-    <p><strong><u>Cijene</u></strong><u><br/></u>Cijene navedene na webu su maloprodajne, sa uključenim PDV-om i povratnom naknadom od 0,50 kn, izražene u eurima (EUR).</p>
-    
-    <p>Prodavatelj ( Vinarija Ivanić  ) je ovlašten mijenjati cijene bez prethodne obavijesti, kao što je ovlašten bez prethodne obavijesti mijenjati cijene isključivo za webshop. Također, Prodavatelj je ovlašten bez prethodne obavijesti odrediti popust, dnevne ili tjedne akcije za pojedine proizvode. Navedene pogodnosti mogu biti isključivo za web kupovinu što će kupcu biti naznačeno prije same kupovine.</p>
-    
 
-    
-    <p><strong><u>Dostava</u></strong></p>
-    
-     
-    <p>Vršimo dostavu na području Republike Hrvatske.</p>
-    
+            <p><strong><u>Dostava</u></strong></p>
 
-    <p>Narudžbe putem interet stranica zaprimaju se svakim danom od 0-24h, obrađuju se nakon evidentirane transakcije, a dostava se vrši putem partnerske dostavne službe.</p>
-    
-    <p>Sve narudžbe zaprimljene radnim danom (ponedjeljak – petak) do 12.00h bit će obrađene isti radni dan, a pošiljka će biti dostavljena najkasnije u roku 72 sata, odnosno tri radna dana. Rok dostave naručenih pošiljki za otoke ovisi o specifičnostima prijevoza na navedene lokacije.</p>
-    
-    <p>Za sve narudžbe vrijednosti iznad 53,00 EUR dostava je besplatna. Za narudžbe vrijednosti do 53,00 EUR, cijena dostave iznosi 5,30 EUR s uključenim PDV-om.</p>
-    
-    <p>Dostava na otoke i područje Dubrovnika uvećava se za 50%.</p>
-    
-    
-    <p><strong><u>Plaćanje</u></strong></p>
-    
-    <p>Platiti je moguće:</p>
-    
-    <p>1. Jednokratno kreditnim karticama (Visa, Mastercard, Maestro) direktno putem našeg web shopa. Uslugu plaćanja kreditnim karticama osigurava CorvusPay, te je sigurnost Vaših podataka zajamčena. Plaćanje se smatra izvršenim u trenutku primitka autorizacije od strane izdavatelja kartice.</p>
-    
-    <p>2. Pouzećem. Plaćanje se smatra izvršenim u trenutku preuzimanja novca od strane dostavne službe.</p>
-    
-    <p>3. Uplatom na transakcijski račun ( općom uplatnicom, virmanom ili e-banking ). Plaćanje se smatra izvršenim kada je uplata vidljiva na transakcijskom računu Prodavatelja.&nbsp;</p>
-    
-    
-    <p><strong><u>Izjava o sigurnosti online plaćanja</u></strong></p>
-    
-    <p>Pri plaćanju na našoj web trgovini koristite CorvusPay – napredni sustav za siguran prihvat platnih kartica putem interneta.</p>
-    
-    <p>CorvusPay osigurava potpunu tajnost Vaših kartičnih podataka već od trenutka kada ih upišete u CorvusPay platni formular. Platni podaci prosljeđuju se šifrirano od Vašeg web preglednika do banke koja je izdala Vašu karticu. Naša trgovina nikada ne dolazi u kontakt s cjelovitim podacima o Vašoj platnoj kartici. Također, podaci su nedostupni čak i djelatnicima CorvusPay sustava. Izolirana jezgra samostalno prenosi i upravlja osjetljivim podacima, čuvajući ih pri tom potpuno sigurnima.</p>
-    
-    <p>Formular za upis platnih podataka osiguran je SSL transportnom šifrom najveće pouzdanosti. Svi skladišteni podaci dodatno su zaštićeni šifriranjem, korištenjem kriptografskog uređaja certificiranog prema FIPS 140-2 Level 3 standardu. CorvusPay zadovoljava sve zahtjeve vezane uz sigurnost <em>on-line&nbsp;</em>plaćanja propisane od strane vodećih kartičnih brandova, odnosno posluje sukladno normi – PCI DSS Level 1 – najviši sigurnosni standard industrije platnih kartica. Pri plaćanju karticama uvrštenim u 3-D Secure program Vaša banka uz valjanost same kartice dodatno potvrđuje i Vaš identitet pomoću tokena ili lozinke.</p>
-    
-    <p>Corvus Info sve prikupljene informacije smatra bankovnom tajnom i tretira ih u skladu s tim. Informacije se koriste isključivo u svrhe za koje su namijenjene. Vaši osjetljivi podaci u potpunosti su sigurni, a njihova privatnost zajamčena je najmodernijim zaštitnim mehanizmima. Prikupljaju se samo podaci nužni za obavljanje posla sukladno propisanim zahtjevnim procedurama za <em>on-line&nbsp;</em>plaćanje.</p>
-    
-    <p>Sigurnosne kontrole i operativne procedure primijenjene na našu infrastrukturu osiguravaju trenutnu pouzdanost CorvusPay sustava. Uz to održavanjem stroge kontrole pristupa, redovitim praćenjem sigurnosti i dubinskim provjerama za sprječavanje ranjivosti mreže te planskim provođenjem odredbi o informacijskoj sigurnosti trajno održavaju i unaprjeđuju stupanj sigurnosti sustava zaštitom Vaših kartičnih podataka.</p>
-    
-  
-    
-    <p><strong><u>Dobna ograničenja</u></strong><u><br/></u>Zabranjena je prodaja alkoholnih pića osobama mlađim od 18 godina. Svaki kupac dužan je na upit dostavljača predočiti dokument kojim dokazuje svoju punoljetnost.</p>
-    
 
-    
-    <p><strong><u>Povrat sredstava</u></strong><u><br/></u>Ukoliko se dogodi da robu koju ste platili trenutno nemamo na lageru, izvršit ćemo povrat sredstava u roku 24 sata. Kontaktirati ćemo Vas da li želite da se ostatak narudžbe šalje. Nastojati ćemo dnevno ažurirati ponudu tako da artikli kojih nemamo budu blokirani za narudžbu kako bi mogućnost propusta bila minimalna.</p>
-    
+            <p>Vršimo dostavu na području Republike Hrvatske.</p>
 
-    
-    <p><strong><u>Izjava o privatnosti</u></strong><u><br/></u>Vinarija Ivanić d.o.o se obvezuje pružati zaštitu osobnim podacima kupaca, na način da prikuplja samo nužne, osnovne podatke o kupcima/korisnicima koji su nužni za ispunjenje naših obveza; informira kupce o načinu korištenja prikupljenih podataka, redovito daje kupcima mogućnost izbora o upotrebi njihovih podataka, uključujući mogućnost odluke žele li ili ne da se njihovo ime ukloni s lista koje se koriste za marketinške kampanje. Svi se podaci o korisnicima strogo čuvaju i dostupni su samo djelatnicima kojima su ti podaci nužni za obavljanje posla. Svi djelatnici Vinarija Ivanić   i poslovni partneri odgovorni su za poštivanje načela zaštite privatnosti.</p>
 
-    
-    <p><strong><u>Prigovori potrošača</u></strong><u><br/></u>Sve eventualne prigovore molimo uputite pismenim putem te ćemo na iste u najkraćem mogućem roku odgovoriti. Adresa je: Vinarija Ivanić  , Sokolovec 16G, 44320 Kutina. Pismeni prigovor možete podnijeti na <a data-fr-linked="true" href="mailto:info@vinoteka.hr">info@vinoteka.hr</a>. Podaci koji su obavezni u prigovoru su: Ime i prezime, br. narudžbe i datum narudžbe.&nbsp;</p>
-    
-    <p>Također Vas molimo da robu pregledate prilikom preuzimanja jer naknadne reklamacije nećemo moći uvažiti.</p>
-    
+            <p>Narudžbe putem interet stranica zaprimaju se svakim danom od 0-24h, obrađuju se nakon evidentirane transakcije, a dostava se vrši putem partnerske dostavne službe.</p>
 
-    
-    <p><strong><u>Upute za jednostrani raskid ugovora (povrat robe)</u></strong><u><br/></u>Potrošač može bez navođenja razloga raskinuti ugovor i vratiti robu u roku 14 dana od kupnje. Trošak prijevoza kod povrata snosi potrošač. Obrazac za jednostrani raskid ugovora možete pronaći na ovom <a href="https://vinoteka.hr/storage/app/media/prigovor-potrosaca.pdf" rel="noopener noreferrer" target="_blank"><strong>linku</strong></a><strong>&nbsp;</strong>te ga ispuniti i poslati na adresu koja piše na obrascu. Potvrdu primitka obavijesti o jednostranom raskidu ugovora dostavit ćemo Vam, bez odgađanja, elektroničkom poštom.</p>
-    
-    <p>Da biste mogli ostvariti pravo na jednostrani raskid ovoga Ugovora, morate nas obavijestiti o svojoj odluci o jednostranom raskidu ugovora prije isteka roka i to nedvosmislenom izjavom poslanom poštom ili elektroničkom poštom, u kojoj ćete navesti svoje ime i prezime, adresu, broj telefona ili adresu elektroničke pošte.</p>
-    
-    <p>Ako jednostrano raskinete ovaj Ugovor, izvršit ćemo povrat novca koji smo od Vas primili, bez odgađanja, a najkasnije u roku od 14 dana od dana kada smo zaprimili Vašu odluku o jednostranom raskidu ugovora.</p>
-    
-    <p>Povrat novca će biti izvršen na isti način na koji ste Vi izvršili uplatu.</p>
-    
-    <p>Povrat novca možemo izvršiti tek nakon što nam roba bude vraćena.</p>
-    
-    <p>Robu treba vratiti na našu adresu (Sokolovec 16G, 44320 Kutina) bez odgađanja, a najkasnije u roku od 14 dana od dana kada ste nam uputili Vašu odluku o jednostranom raskidu ugovora.</p>
-    
-    <p>
-        <br/>
-    </p>
-    
-    <p><strong><u>Materijalni nedostaci</u></strong><u><br/></u>Materijalne nedostatke rješavamo sukladno odredbama Zakona o obveznim odnosima</p>
-    
-    <p>Korisnik ima pravo na povrat ili reklamaciju proizvoda u roku od osam (8) kalendarskih dana od dana dostave proizvoda u slučaju:</p>
-    
-    <ul >
-        <li>
-            kada je isporučen proizvod/proizvodi koji nije naručen;</li>
-        <li>
-            kada isporučeni proizvod ima oštećenja koja nisu nastala u transportu.</li>
-    </ul>
-    
-    <p>
-        <br/>
-    </p>
-  </>
-   );
+            <p>Sve narudžbe zaprimljene radnim danom (ponedjeljak – petak) do 12.00h bit će obrađene isti radni dan, a pošiljka će biti dostavljena najkasnije u roku 72 sata, odnosno tri radna dana. Rok dostave naručenih pošiljki za otoke ovisi o specifičnostima prijevoza na navedene lokacije.</p>
+
+            <p>Za sve narudžbe vrijednosti iznad 53,00 EUR dostava je besplatna. Za narudžbe vrijednosti do 53,00 EUR, cijena dostave iznosi 5,30 EUR s uključenim PDV-om.</p>
+
+            <p>Dostava na otoke i područje Dubrovnika uvećava se za 50%.</p>
+
+
+            <p><strong><u>Plaćanje</u></strong></p>
+
+            <p>Platiti je moguće:</p>
+
+            <p>1. Jednokratno kreditnim karticama (Visa, Mastercard, Maestro) direktno putem našeg web shopa. Uslugu plaćanja kreditnim karticama osigurava CorvusPay, te je sigurnost Vaših podataka zajamčena. Plaćanje se smatra izvršenim u trenutku primitka autorizacije od strane izdavatelja kartice.</p>
+
+            <p>2. Pouzećem. Plaćanje se smatra izvršenim u trenutku preuzimanja novca od strane dostavne službe.</p>
+
+            <p>3. Uplatom na transakcijski račun ( općom uplatnicom, virmanom ili e-banking ). Plaćanje se smatra izvršenim kada je uplata vidljiva na transakcijskom računu Prodavatelja.&nbsp;</p>
+
+
+            <p><strong><u>Izjava o sigurnosti online plaćanja</u></strong></p>
+
+            <p>Pri plaćanju na našoj web trgovini koristite CorvusPay – napredni sustav za siguran prihvat platnih kartica putem interneta.</p>
+
+            <p>CorvusPay osigurava potpunu tajnost Vaših kartičnih podataka već od trenutka kada ih upišete u CorvusPay platni formular. Platni podaci prosljeđuju se šifrirano od Vašeg web preglednika do banke koja je izdala Vašu karticu. Naša trgovina nikada ne dolazi u kontakt s cjelovitim podacima o Vašoj platnoj kartici. Također, podaci su nedostupni čak i djelatnicima CorvusPay sustava. Izolirana jezgra samostalno prenosi i upravlja osjetljivim podacima, čuvajući ih pri tom potpuno sigurnima.</p>
+
+            <p>Formular za upis platnih podataka osiguran je SSL transportnom šifrom najveće pouzdanosti. Svi skladišteni podaci dodatno su zaštićeni šifriranjem, korištenjem kriptografskog uređaja certificiranog prema FIPS 140-2 Level 3 standardu. CorvusPay zadovoljava sve zahtjeve vezane uz sigurnost <em>on-line&nbsp;</em>plaćanja propisane od strane vodećih kartičnih brandova, odnosno posluje sukladno normi – PCI DSS Level 1 – najviši sigurnosni standard industrije platnih kartica. Pri plaćanju karticama uvrštenim u 3-D Secure program Vaša banka uz valjanost same kartice dodatno potvrđuje i Vaš identitet pomoću tokena ili lozinke.</p>
+
+            <p>Corvus Info sve prikupljene informacije smatra bankovnom tajnom i tretira ih u skladu s tim. Informacije se koriste isključivo u svrhe za koje su namijenjene. Vaši osjetljivi podaci u potpunosti su sigurni, a njihova privatnost zajamčena je najmodernijim zaštitnim mehanizmima. Prikupljaju se samo podaci nužni za obavljanje posla sukladno propisanim zahtjevnim procedurama za <em>on-line&nbsp;</em>plaćanje.</p>
+
+            <p>Sigurnosne kontrole i operativne procedure primijenjene na našu infrastrukturu osiguravaju trenutnu pouzdanost CorvusPay sustava. Uz to održavanjem stroge kontrole pristupa, redovitim praćenjem sigurnosti i dubinskim provjerama za sprječavanje ranjivosti mreže te planskim provođenjem odredbi o informacijskoj sigurnosti trajno održavaju i unaprjeđuju stupanj sigurnosti sustava zaštitom Vaših kartičnih podataka.</p>
+
+
+
+            <p><strong><u>Dobna ograničenja</u></strong><u><br /></u>Zabranjena je prodaja alkoholnih pića osobama mlađim od 18 godina. Svaki kupac dužan je na upit dostavljača predočiti dokument kojim dokazuje svoju punoljetnost.</p>
+
+
+
+            <p><strong><u>Povrat sredstava</u></strong><u><br /></u>Ukoliko se dogodi da robu koju ste platili trenutno nemamo na lageru, izvršit ćemo povrat sredstava u roku 24 sata. Kontaktirati ćemo Vas da li želite da se ostatak narudžbe šalje. Nastojati ćemo dnevno ažurirati ponudu tako da artikli kojih nemamo budu blokirani za narudžbu kako bi mogućnost propusta bila minimalna.</p>
+
+
+
+            <p><strong><u>Izjava o privatnosti</u></strong><u><br /></u>Vinarija Ivanić d.o.o se obvezuje pružati zaštitu osobnim podacima kupaca, na način da prikuplja samo nužne, osnovne podatke o kupcima/korisnicima koji su nužni za ispunjenje naših obveza; informira kupce o načinu korištenja prikupljenih podataka, redovito daje kupcima mogućnost izbora o upotrebi njihovih podataka, uključujući mogućnost odluke žele li ili ne da se njihovo ime ukloni s lista koje se koriste za marketinške kampanje. Svi se podaci o korisnicima strogo čuvaju i dostupni su samo djelatnicima kojima su ti podaci nužni za obavljanje posla. Svi djelatnici Vinarija Ivanić   i poslovni partneri odgovorni su za poštivanje načela zaštite privatnosti.</p>
+
+
+            <p><strong><u>Prigovori potrošača</u></strong><u><br /></u>Sve eventualne prigovore molimo uputite pismenim putem te ćemo na iste u najkraćem mogućem roku odgovoriti. Adresa je: Vinarija Ivanić  , Sokolovec 16G, 44320 Kutina. Pismeni prigovor možete podnijeti na <a data-fr-linked="true" href="mailto:info@vinoteka.hr">info@vinoteka.hr</a>. Podaci koji su obavezni u prigovoru su: Ime i prezime, br. narudžbe i datum narudžbe.&nbsp;</p>
+
+            <p>Također Vas molimo da robu pregledate prilikom preuzimanja jer naknadne reklamacije nećemo moći uvažiti.</p>
+
+
+
+            <p><strong><u>Upute za jednostrani raskid ugovora (povrat robe)</u></strong><u><br /></u>Potrošač može bez navođenja razloga raskinuti ugovor i vratiti robu u roku 14 dana od kupnje. Trošak prijevoza kod povrata snosi potrošač. Obrazac za jednostrani raskid ugovora možete pronaći na ovom <a href="https://vinoteka.hr/storage/app/media/prigovor-potrosaca.pdf" rel="noopener noreferrer" target="_blank"><strong>linku</strong></a><strong>&nbsp;</strong>te ga ispuniti i poslati na adresu koja piše na obrascu. Potvrdu primitka obavijesti o jednostranom raskidu ugovora dostavit ćemo Vam, bez odgađanja, elektroničkom poštom.</p>
+
+            <p>Da biste mogli ostvariti pravo na jednostrani raskid ovoga Ugovora, morate nas obavijestiti o svojoj odluci o jednostranom raskidu ugovora prije isteka roka i to nedvosmislenom izjavom poslanom poštom ili elektroničkom poštom, u kojoj ćete navesti svoje ime i prezime, adresu, broj telefona ili adresu elektroničke pošte.</p>
+
+            <p>Ako jednostrano raskinete ovaj Ugovor, izvršit ćemo povrat novca koji smo od Vas primili, bez odgađanja, a najkasnije u roku od 14 dana od dana kada smo zaprimili Vašu odluku o jednostranom raskidu ugovora.</p>
+
+            <p>Povrat novca će biti izvršen na isti način na koji ste Vi izvršili uplatu.</p>
+
+            <p>Povrat novca možemo izvršiti tek nakon što nam roba bude vraćena.</p>
+
+            <p>Robu treba vratiti na našu adresu (Sokolovec 16G, 44320 Kutina) bez odgađanja, a najkasnije u roku od 14 dana od dana kada ste nam uputili Vašu odluku o jednostranom raskidu ugovora.</p>
+
+            <p>
+                <br />
+            </p>
+
+            <p><strong><u>Materijalni nedostaci</u></strong><u><br /></u>Materijalne nedostatke rješavamo sukladno odredbama Zakona o obveznim odnosima</p>
+
+            <p>Korisnik ima pravo na povrat ili reklamaciju proizvoda u roku od osam (8) kalendarskih dana od dana dostave proizvoda u slučaju:</p>
+
+            <ul >
+                <li>
+                    kada je isporučen proizvod/proizvodi koji nije naručen;</li>
+                <li>
+                    kada isporučeni proizvod ima oštećenja koja nisu nastala u transportu.</li>
+            </ul>
+
+            <p>
+                <br />
+            </p>
+        </>
+    );
 }
 
 export default Terms;
