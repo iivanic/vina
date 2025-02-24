b3307986c2ace76e859c97f7f271471bdfe8d1ab
Igor Ivanic <igor_ivanic@hotmail.com>
2025-02-24 19:08:19 +0100

    ..

--- vina.Server/LoggerDatabaseProvider .cs
+++ vina.Server/LoggerDatabaseProvider .cs
@@ -46,18 +46,36 @@ public class LoggerDatabaseProvider : ILoggerProvider
                 Exception = exception == null ? "" : exception.ToString(),
                 LogTimestamp = DateTime.Now
             };
-            await new DBcs.DBcs(_connectionString).RunNonQueryAsync(DBLog.InsertText, log);
+            DBcs.DBcs db = new DBcs.DBcs(_connectionString);
+
+            var ret = db.RunScalarAsync(@"
+                    select 
+                        count(*)
+                    from
+                        information_schema.tables t  
+                    where 
+                        t.table_schema = 'public' 
+                    and 
+                        table_type='BASE TABLE'
+                    and 
+                        table_name='logs';")
+                        .GetAwaiter().GetResult(); 
+            if(ret == null)
+                return;
+            if((Int64)ret>0)
+                await db.RunNonQueryAsync(DBLog.InsertText, log);
+         
         }
 
-        IDisposable ILogger.BeginScope<TState>(TState state) 
+        IDisposable ILogger.BeginScope<TState>(TState state)
         {
             return new NoopDisposable();
         }
 
         void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
         {
-                 RecordMsg(logLevel, eventId, state, exception, formatter).GetAwaiter().GetResult();
-     
+            RecordMsg(logLevel, eventId, state, exception, formatter).GetAwaiter().GetResult();
+
         }
 
         private class NoopDisposable : IDisposable
